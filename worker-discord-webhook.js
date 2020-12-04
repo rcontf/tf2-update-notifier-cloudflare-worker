0c0d8b1ff69f3c60386709f92017b46871be2f3a
c43721 <rlahman.game@gmail.com>
2020-12-04 15:32:09 -0600

    Add discord webhook support

---
+++ worker-discord-webhook.js
@@ -0,0 +1,52 @@
+const url = "https://api.steampowered.com/ISteamApps/UpToDateCheck/v1/?appid=440&version="
+const WEBHOOK_URL = "YOUR WEBHOOK LINK HERE"
+
+async function handleRequest() {
+  const init = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+
+  const [constKvVersion, tf2KvVersion] = await Promise.all([
+    TF2_UPDATE.get("CONSTANT_VERSION"),
+    TF2_UPDATE.get("VERSION")
+  ])
+
+  const resFromTf2Api = await fetch(url + constKvVersion, init)
+  const { response } = await resFromTf2Api.json()
+
+  const resJson = {
+    versionTested: constKvVersion,
+    currentKvVersion: tf2KvVersion
+  }
+
+  if (response.required_version.toString() === tf2KvVersion.toString()) {
+    return new Response(JSON.stringify(resJson), init)
+  } else {
+    await updateVersionInKv(response.required_version)
+    //send patch, post request to servers here...
+
+    //send webhook with content only if there was an update
+    await sendWebhook({ content: "there was tf2 update" });
+    return new Response(JSON.stringify({ ...resJson, updated: true }), init)
+  }
+}
+
+async function updateVersionInKv(newVersion) {
+  await TF2_UPDATE.put("VERSION", newVersion)
+}
+
+async function sendWebhook(data) {
+  await fetch(WEBHOOK_URL, {
+    body: JSON.stringify(data),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+}
+
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest())
+})
