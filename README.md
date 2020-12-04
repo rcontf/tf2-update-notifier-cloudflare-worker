9075bb69d69fcc8542ddbd075a49ac53f9892602
c43721 <rlahman.game@gmail.com>
2020-12-04 14:52:56 -0600

    Add README and worker.js

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # tf2-update-notifier-cloudflare-worker
- Sends a request to Steam's API to be notified if there was a TF2 update.
+
+Sends a request to Steam's API to be notified if there was a TF2 update.
+
+# Usage
+
+- Create a cloudflare worker application
+- Create a KV container
+- Inside the KV, you need 2 items:
+
+```
+CONSTANT_VERSION
+VERSION
+```
+
+- Link them to the worker and name them TF2_UPDATE
+- Set the value of the `CONSTANT_VERSION` to an older version (control) and VERSION to either the newest or an old version.
+- Done! `VERSION` will be automatically updated if there was an update.
+
+# Expandability
+
+You can use this worker to call your API to trigger a build of an image or to automatically update a TF2 server through a backend. This only parses the latest update and notifies you of such update.
---
+++ worker.js
@@ -0,0 +1,39 @@
+const url = "https://api.steampowered.com/ISteamApps/UpToDateCheck/v1/?appid=440&version="
+
+async function handleRequest() {
+  const init = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+
+  const [constKvVersion, tf2KvVersion] = await Promise.all([
+    TF2_UPDATE.get("CONSTANT_VERSION"),
+    TF2_UPDATE.get("VERSION")
+  ])
+
+  const resFromTf2Api = await fetch(url + constKvVersion, init)
+  const { response } = await resFromTf2Api.json()
+
+  const resJson = {
+    versionTested: constKvVersion,
+    currentKvVersion: tf2KvVersion
+  }
+
+  if (response.required_version === tf2KvVersion) {
+    return new Response(JSON.stringify(resJson), init)
+  } else {
+    await updateVersionInKv(response.required_version)
+    //send patch, post request to servers here...
+
+    return new Response(JSON.stringify({ ...resJson, updated: true }), init)
+  }
+}
+
+async function updateVersionInKv(newVersion) {
+  await TF2_UPDATE.put("VERSION", newVersion)
+}
+
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest())
+})
