0b0b50934baef73f742d2d83f2b20b40b7a9e167
c43721 <rlahman.game@gmail.com>
2020-12-13 16:04:09 -0600

    Compare the strings instead of string and number

--- worker.js
+++ worker.js
@@ -16,7 +16,7 @@ async function handleRequest() {
     versionTested: constKvVersion
   }
 
-  if (response.required_version === constKvVersion) {
+  if (response.required_version.toString() === constKvVersion.toString()) {
     return new Response(JSON.stringify(resJson), init)
   } else {
     await updateVersionInKv(response.required_version)
