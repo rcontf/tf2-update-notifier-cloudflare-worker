d123a2cc7028f0ba175dc525149c5f3da55a4984
c43721 <rlahman.game@gmail.com>
2021-09-24 19:56:25 -0500

    feat: add all-in-one

---
+++ worker-all-in-one.js
@@ -0,0 +1,62 @@
+const STEAM_API_URL = "https://api.steampowered.com/ISteamApps/UpToDateCheck/v1/?appid=440&version=0"
+
+const headers = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+}
+
+async function updateVersionInKv(newVersion) {
+  await TF2_UPDATE.put("VERSION", newVersion)
+}
+
+async function sendWebhook() {
+  // Edit to hearts content
+  const formBody = {
+    embeds: [{
+      description: "A TF2 update has been released.",
+      color: 15105570,
+      timestamp: new Date(),
+      footer: {
+        text: "TF2 Update Worker"
+      }
+    }]
+  }
+
+  // Edit DISCORD_WEBHOOK_1 with whatever your ENV variable is
+  await fetch(DISCORD_WEBHOOK_1, {
+    body: JSON.stringify(formBody),
+    method: "POST",
+    headers
+  })
+}
+
+async function testForUpdate() {
+  const constKvVersion = await TF2_UPDATE.get("VERSION")
+  const resFromTf2Api = await fetch(STEAM_API_URL, headers)
+  const { response } = await resFromTf2Api.json()
+
+  const isNewUpdate = response.required_version.toString() !== constKvVersion.toString()
+
+  if (isNewUpdate) {
+    await Promise.all([
+      updateVersionInKv(response.required_version), sendWebhook()
+    ])
+
+    return true
+  }
+
+  return false
+}
+
+async function handleRequest() {
+  const hasUpdate = await testForUpdate()
+
+  if (!hasUpdate) return new Response(JSON.stringify({ update: false }), headers)
+
+  return new Response(JSON.stringify({ update: true }), headers)
+}
+
+addEventListener("fetch", event => event.respondWith(handleRequest()))
+
+addEventListener("scheduled", event => event.waitUntil(testForUpdate()))
