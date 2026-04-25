00bee8c1911d4c4d2ecf427daa7e4182c80c1552
c43721 <rlahman.game@gmail.com>
2026-04-25 14:21:26 -0500

    feat: comprehensive unit tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -38,4 +38,60 @@ describe("tf2 update worker", () => {
 
 		expect(fetchMock).toHaveBeenCalledOnce()
 	});
+
+	it("responds false when no new version is available", async () => {
+		await env.TF2_UPDATE.put("VERSION", "1");
+
+		const fetchMock = vi.fn().mockResolvedValue({
+			ok: true,
+			json: () => Promise.resolve({ response: { required_version: 1 } })
+		})
+
+		vi.stubGlobal('fetch', fetchMock);
+
+		const request = new IncomingRequest("https://example.com");
+
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+
+		await waitOnExecutionContext(ctx);
+
+		const data = await response.json();
+
+		expect(data).toMatchInlineSnapshot({ update: false }, `
+			{
+			  "update": false,
+			}
+		`);
+
+		expect(fetchMock).toHaveBeenCalledOnce()
+	});
+
+	it("responds true when new version is available", async () => {
+		await env.TF2_UPDATE.put("VERSION", "1");
+
+		const fetchMock = vi.fn().mockResolvedValue({
+			ok: true,
+			json: () => Promise.resolve({ response: { required_version: 2 } })
+		})
+
+		vi.stubGlobal('fetch', fetchMock);
+
+		const request = new IncomingRequest("https://example.com");
+
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+
+		await waitOnExecutionContext(ctx);
+
+		const data = await response.json();
+
+		expect(data).toMatchInlineSnapshot({ update: true }, `
+			{
+			  "update": true,
+			}
+		`);
+
+		expect(fetchMock).toHaveBeenCalledOnce()
+	});
 });
